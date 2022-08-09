74af5bd7448f821b89986ba8bcab2c5d7e080f10
Hamma Azarok <hammaagazarok@gmail.com>
2022-08-10 00:06:52 +0200

    add components

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -25,25 +25,177 @@ const Calculator = () => {
   return (
     <div className="calculator">
       <div className="cal-item screen">0</div>
-      <div className="cal-item" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="0">AC</div>
-      <div className="cal-item" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-1">+/-</div>
-      <div className="cal-item" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-2">%</div>
-      <div className="cal-item orange" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-3">÷</div>
-      <div className="cal-item" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-4">7</div>
-      <div className="cal-item" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-5">8</div>
-      <div className="cal-item" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-6">9</div>
-      <div className="cal-item orange" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-7">x</div>
-      <div className="cal-item" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-8">4</div>
-      <div className="cal-item" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-9">5</div>
-      <div className="cal-item" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-10">6</div>
-      <div className="cal-item orange" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-11">-</div>
-      <div className="cal-item" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-12">1</div>
-      <div className="cal-item" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-13">2</div>
-      <div className="cal-item" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-14">3</div>
-      <div className="cal-item orange" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-15">+</div>
-      <div className="cal-item zero" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-16">0</div>
-      <div className="cal-item" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-17">.</div>
-      <div className="cal-item orange" onClick={performClick} onKeyDown={performClick} role="button" tabIndex="-18">=</div>
+      <div
+        className="cal-item"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="0"
+      >
+        AC
+      </div>
+      <div
+        className="cal-item"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-1"
+      >
+        +/-
+      </div>
+      <div
+        className="cal-item"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-2"
+      >
+        %
+      </div>
+      <div
+        className="cal-item orange"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-3"
+      >
+        ÷
+      </div>
+      <div
+        className="cal-item"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-4"
+      >
+        7
+      </div>
+      <div
+        className="cal-item"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-5"
+      >
+        8
+      </div>
+      <div
+        className="cal-item"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-6"
+      >
+        9
+      </div>
+      <div
+        className="cal-item orange"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-7"
+      >
+        x
+      </div>
+      <div
+        className="cal-item"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-8"
+      >
+        4
+      </div>
+      <div
+        className="cal-item"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-9"
+      >
+        5
+      </div>
+      <div
+        className="cal-item"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-10"
+      >
+        6
+      </div>
+      <div
+        className="cal-item orange"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-11"
+      >
+        -
+      </div>
+      <div
+        className="cal-item"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-12"
+      >
+        1
+      </div>
+      <div
+        className="cal-item"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-13"
+      >
+        2
+      </div>
+      <div
+        className="cal-item"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-14"
+      >
+        3
+      </div>
+      <div
+        className="cal-item orange"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-15"
+      >
+        +
+      </div>
+      <div
+        className="cal-item zero"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-16"
+      >
+        0
+      </div>
+      <div
+        className="cal-item"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-17"
+      >
+        .
+      </div>
+      <div
+        className="cal-item orange"
+        onClick={performClick}
+        onKeyDown={performClick}
+        role="button"
+        tabIndex="-18"
+      >
+        =
+      </div>
     </div>
   );
 };
